eb47037ab5c76c930c5081761aca3f88c1f9de4c
Jay Bennett <bennettjc1978@gmail.com>
2017-01-26 21:43:06 -0600

    Update main.py to include html escaping and cleaner page rendering.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import webapp2
 import caesar
+import cgi
 
 
 page_header = """
@@ -9,6 +10,7 @@ page_header = """
     <title>web caesar</title>
 </head>
 <body>
+<h2>Web Caesar</h2>
 """
 
 page_footer = """
@@ -16,19 +18,27 @@ page_footer = """
 </html>
 """
 
+def make_page(text_content):
+        page = ''
+        sentence_1 = "<p>Enter the text you'd like to encrypt:</p>"
+        rotation = "<br><br><label>Enter rotation:<br><br><input type='number' name='rotation' min='1' max='200' style='width:80px;'/></label><br>"
+        text_area = "<textarea name='text' style='height:100px;width:400px;'>" + text_content + "</textarea>"
+        submit =  "<br><input type='submit' value='Encrypt'/>"
+        form = "<form method='post'>" + text_area + rotation + submit + "</form>"
+        page = page_header + sentence_1 + "<br>" + form + page_footer
+        return page
+
 
 
 class MainRequestHandler(webapp2.RequestHandler):
     def get(self):
-        page = ''
-        sentence_1 = "<p>Enter the text you'd like to encrypt:</p>"
-        rotation = "<br><input type='number' name='rotation-number' min='1' max='200'/>"
-        text_area = "<textarea name='text' style='height:100px;width:400px;'></textarea>"
-        submit =  "<br><input type='submit' value='Encrypt'/>"
-        form = "<form method='post'>" + rotation + text_area + submit + "</form>"
-        page = page_header + sentence_1 + "<br>" + form + page_footer
+        page = make_page("")
         self.response.write(page)
 
+    def post(self):
+        message = cgi.escape(caesar.encrypt(self.request.get('text'), int(self.request.get('rotation'))))
+        page = make_page(message)
+        self.response.write(page)
 
 
 
